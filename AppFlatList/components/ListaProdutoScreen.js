78157d4771819a82892efe8ceba8bd296f335b6b
JoseMaksud <jf9718830@gmail.com>
2024-09-03 20:37:00 -0300

    Adicionando detalhes ao produto

--- AppFlatList/components/ListaProdutoScreen.js
+++ AppFlatList/components/ListaProdutoScreen.js
@@ -21,31 +21,33 @@ import Produtos from '../data/Produtos'
 
 export default props => {
 
-// Function to read title
-const Item = ({elemento: item}) => {
-    return(   
-    <List.Item
-        title={item.titulo}
-        description={item.descricao}
-        // left={props => <List.Icon {...props} icon="folder" />}
-        left={props => 
-            <Image {...props} 
-                style={styles.tinyLogo} 
-                source={
-                    {
-                        uri: item.urlImagem
-                    }
-                }/>}
-    />
-)};
+    // Function to read title
+    const Item = ({ elemento: item }) => {
+        return (
+            <List.Item
+                title={item.titulo}
+                description={item.descricao}
+                // left={props => <List.Icon {...props} icon="folder" />}
+                left={props =>
+                    <Image {...props}
+                        style={styles.tinyLogo}
+                        source={
+                            {
+                                uri: item.urlImagem
+                            }
+                        } />}
+                onPress={() => props.navigation.navigate("DetalheProduto", {id: item.id})}
+            />
+        )
+    };
 
-// App
+    // App
     return (
         <View style={styles.container}>
             <FlatList
-                data={ Produtos }
-                renderItem={({ item }) =><Item elemento={ item } /> }
-                keyExtractor={ item => item.id }
+                data={Produtos}
+                renderItem={({ item }) => <Item elemento={item} />}
+                keyExtractor={item => item.id}
             />
         </View>
     );
@@ -54,13 +56,13 @@ const Item = ({elemento: item}) => {
 // Style
 const styles = StyleSheet.create({
     container: {
-        flex:      1,
+        flex: 1,
         marginTop: StatusBar.currentHeight || 0,
     },
     item: {
         backgroundColor: '#f9c2ff',
-        padding:          20,
-        marginVertical:   8,
+        padding: 20,
+        marginVertical: 8,
         marginHorizontal: 16,
     },
     title: {
